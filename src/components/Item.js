767393871078dbb4c2b0a48ead0ec6013acaa46d
Justin Bigishiro <justin.rvd@gmail.com>
2023-06-03 05:38:08 -0500

    adds myfile

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [buttonColor, setButtonColor] = useState(true);
+  const [lineThrough, setLinethrough] = useState(false);
+ 
+
+  function handleClick(){
+    setButtonColor(!buttonColor)
+    setLinethrough(!lineThrough)
+  }
+   
+  const lineThoughClass=lineThrough? "in-cart": ""
+  const buttonClass = buttonColor ? "add" : "remove";
+
   return (
-    <li className="">
+    <li className={lineThoughClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClass} onClick={handleClick}>
+        {buttonColor ? "Add to Cart" : "Remove From Cart"}
+      </button>
     </li>
   );
 }
